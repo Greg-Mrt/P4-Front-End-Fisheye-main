81a98785d5f9631dfc99c1ce9eb869c5c5909913
Greg-Mrt <107677202+Greg-Mrt@users.noreply.github.com>
2022-11-21 19:52:50 +0100

    stape 2 and 3 added

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, id, tagline } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,12 +7,21 @@ function photographerFactory(data) {
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        const lien = document.createElement('a');
+        lien.setAttribute("href","photographer.html?id="+id)
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h3 = document.createElement('h3');
+        h3.textContent = `${city}, ${country}`;
+        const p = document.createElement('p');
+        p.textContent = tagline;
+        lien.appendChild(img);
+        lien.appendChild(h2);
+        article.appendChild(lien);
+        article.appendChild(h3);
+        article.appendChild(p);
         return (article);
     }
     return { name, picture, getUserCardDOM }
