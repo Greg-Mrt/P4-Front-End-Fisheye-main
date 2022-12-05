bc0412c654c2226aa0430772490cf31efd2a2c14
Greg-Mrt <107677202+Greg-Mrt@users.noreply.github.com>
2022-12-05 23:56:13 +0100

    fixes in mantorat cession

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,33 +1,57 @@
 //Mettre le code JavaScript lié à la page photographer.html
 let params = (new URL(document.location)).searchParams;
-let id = params.get('id');
+let idUrl = params.get('id');
 
 async function getPhotographers() {
-const response = await fetch('/data/photographers.json')
-const fichierjson  = await response.json();
-return fichierjson.photographers;
+  const response = await fetch('/data/photographers.json')
+  const fichierjson  = await response.json();
+  return fichierjson.photographers;
+}
+async function getMedia() {
+  const response = await fetch('/data/photographers.json')
+  const fichierjson  = await response.json();
+  return fichierjson.media;
 }
 
 async function init(){
-const photographers = await getPhotographers();
-const profil = photographers.find(photo => photo.id == id);
-console.log(profil);
-const photographerInfo = document.querySelector(".photographer_section");
+  const photographers = await getPhotographers();
+// je récupère le photographe lié à l'id de l'URL
+  const profil = photographers.find((photo) => 
+    photo.id == idUrl
+  );
+  console.log(profil);
+  // si pas (ou mauvais) id je reviens sur la page index
+  if(profil==undefined){
+    window.location.href="/"
+  }
+  const photographerInfo = document.querySelector(".photographer_section");
   const photographeHeader = document.createElement("article");
-  const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${profil.portrait}`;
   const img = document.createElement('img');
   img.setAttribute("src", picture);
-  let info = `<h1 class ="photographer_name">${id[0].name}</h1>
-            <p class="photographer_location>${id[0].city} , ${id[0].country}</p>
-            <p class="photographer_devise>${id[0].tagline}</p>
-          <div class="profilePicture"> <img src="assets/photographers/${id[0].portrait}"></div>
+  let info = `<h1 class ="photographer_name">${profil.name}</h1>
+            <p class="photographer_location>${profil.city} , ${profil.country}</p>
+            <p class="photographer_devise>${profil.tagline}</p>
+          <div class="profilePicture"> <img src="assets/photographers/${profil.portrait}"></div>
             `;
   photographerInfo.appendChild(photographeHeader);
   photographeHeader.innerHTML = info;
 
-if(profil==undefined){
-    window.location.href="/"
-}
+  const mediaSource = await getMedia();
+  // je récupère les médias liés à l'id de l'URL
+  let medias = mediaSource.filter((m) => 
+    m.photographerId == idUrl
+  )
+  medias = medias.sort(function (a, b) {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+    console.log(medias);
 }
 
 init();
