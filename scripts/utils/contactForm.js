088e14eb521fb834ed6472739b146e46fa422626
Greg-Mrt <107677202+Greg-Mrt@users.noreply.github.com>
2023-01-03 08:09:41 +0100

    change name in photographer files

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -15,7 +15,7 @@ console.log('fermeture');
 //check "prénom" data
 function FirstName () {
       let inputValue = document.getElementById("prenom").value;
-      if (inputValue !== null) {
+      if (inputValue !== "") {
         return true;
     }   
       else {
@@ -26,7 +26,7 @@ function FirstName () {
   //check "nom de famille" data
   function LastName () {
     let inputValue = document.getElementById("nom").value;
-    if (inputValue !== null) {
+    if (inputValue !== "") {
       return true;
   }   
     else {
@@ -37,7 +37,7 @@ function FirstName () {
   //check "email" data
   function Email () {
     let inputValue = document.getElementById("email").value;
-    if (inputValue !== null) {
+    if (inputValue !== "") {
       return true;
   }   
     else {
@@ -47,7 +47,7 @@ function FirstName () {
   
 function message () {
     let inputValue = document.getElementById("message").value;
-    if (inputValue !== null) {
+    if (inputValue !== "") {
       return true;
   }   
     else {
@@ -56,12 +56,10 @@ function message () {
 }
   
     // fonction pour valider le formulaire
-  document.querySelector("modal").addEventListener("submit", (e) => {
+  document.querySelector("#submit").addEventListener("click", (e) => {
     // "annuler" le comportement par défaut
     e.preventDefault();
-  
-  viderErreur();
-    
+      
   
     // vérifier un par un tous les champs
     const verifFirstName = FirstName();
@@ -73,62 +71,13 @@ function message () {
     // si tout est vrai 
     if(verifFirstName && verifLastName && verifEmail && verifMessage)
     {
-      // j'affiche le message de succès
-      document.querySelector("form").style.display="none";
-      let message=document.createElement("p");
-      message.innerHTML="Merci ! Votre inscription est validée.";
-      message.style.color="red";
-      message.style.textAlign="center";
-      message.style.marginTop="100px";
-      message.style.marginBottom="100px";
-      message.style.marginRight="40px";
-      message.style.marginLeft="40px";
-      message.style.fontSize="25px";
-      document.querySelector(".modal-body").appendChild(message);
+      console.log(document.getElementById("prenom").value);
+      console.log(document.getElementById("nom").value);
+      console.log(document.getElementById("email").value);
+      console.log(document.getElementById("message").value);
     }
-    // si j'ai au moins un faux
-    else
-    {
-      if(!verifFirstName)
-      {
-        document.querySelector("#first").parentElement.setAttribute("data-error", "Saisissez un prénom valide")
-        document.querySelector("#first").parentElement.setAttribute("data-error-visible", true)
-      }
-  
-      if(!verifLastName)
-      {
-        document.querySelector("#last").parentElement.setAttribute("data-error", "Saisissez un nom valide")
-        document.querySelector("#last").parentElement.setAttribute("data-error-visible", true)
-      }
-  
-      if(!verifEmail)
-      {
-        document.querySelector("#email").parentElement.setAttribute("data-error", "Saisissez un email valide")
-        document.querySelector("#email").parentElement.setAttribute("data-error-visible", true)
-      }
-  
-      if(!verifMessage)
-      {
-        document.querySelector("#message").parentElement.setAttribute("data-error", "Saisissez une date de naissance")
-        document.querySelector("#message").parentElement.setAttribute("data-error-visible", true)
-      }
-
+    else{
+      console.log("Il y a une erreur");
     }
-  
   })
   
-  // vider les erreurs
-    function viderErreur() {
-      document.querySelector("#first").parentElement.setAttribute("data-error", "")
-      document.querySelector("#first").parentElement.setAttribute("data-error-visible", false)
-  
-      document.querySelector("#last").parentElement.setAttribute("data-error", "")
-      document.querySelector("#last").parentElement.setAttribute("data-error-visible", false)
-  
-      document.querySelector("#email").parentElement.setAttribute("data-error", "")
-      document.querySelector("#email").parentElement.setAttribute("data-error-visible", false)
-  
-      document.querySelector("#message").parentElement.setAttribute("data-error", "")
-      document.querySelector("#message").parentElement.setAttribute("data-error-visible", false)
-  
-    }
---
+++ scripts/utils/lightbox.js
@@ -0,0 +1,13 @@
+function displayLightbox() {
+    const lightbox = document.getElementById("lightbox");
+	lightbox.style.display = "block";
+}
+
+console.log('ouverture');
+
+function closeLightbox() {
+    const lightbox = document.getElementById("lightbox");
+    lightbox.style.display = "none";
+}
+
+console.log('fermeture');
